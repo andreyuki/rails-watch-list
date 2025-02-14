0808958e003b0be4e0ce589b9ed22353cea07997
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-14 12:19:35 +0900

    teste prod

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import SplideController from "./splide_controller";
 
 const application = Application.start()
-application.register("splide", SplideController);
 
 // Configure Stimulus development experience
 application.debug = false
