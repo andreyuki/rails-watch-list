ed050f357c0b027e96a8702e301eea8ce3564412
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-14 11:50:38 +0900

    testing new ways

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import "controllers/splide_controller"
 // import "@splidejs/splide/dist/css/splide.min.css";
